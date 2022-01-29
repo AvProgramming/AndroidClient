721b93e9402eb383475ccfd71d19b18119305743
AvProphet <olegarx2000@gmail.com>
2022-01-29 19:26:14 +0100

    Fixed product name field

--- app/src/main/java/com/example/restaurant/model/Product.java
+++ app/src/main/java/com/example/restaurant/model/Product.java
@@ -9,7 +9,7 @@ public class Product {
     @SerializedName("id")
     @Expose
     private Long id;
-    @SerializedName("product_name")
+    @SerializedName("name")
     @Expose
     private String productName;
     @SerializedName("price")
